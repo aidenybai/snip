9a7731830658ba045bd2e39af74f6eb99e7aa21c
abai <mail.contraband@gmail.com>
2020-08-02 15:17:28 -0700

    Have inactivity monitor

--- server/routes/handlers/root.js
+++ server/routes/handlers/root.js
@@ -12,7 +12,17 @@ module.exports = class Root {
 
     router.get('/*', async (req, res) => {
       const url = await Snip.findOne({ id: req.url.slice(1) });
-      res.redirect(301, url ? Base64.decode(url.url) : 'https://snip.ml');
+      if (url) {
+        await Snip.findOneAndUpdate(
+          { id: req.url.slice(1) },
+          {
+            expireAt: new Date(Date.now() + 604800000).toISOString(),
+          }
+        );
+        res.redirect(301, Base64.decode(url.url));
+      } else {
+        res.redirect(301, 'https://snip.ml');
+      }
     });
 
     router.use('*', (req, res) => {
