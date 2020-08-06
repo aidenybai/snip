a776433c4fefc91af332929b29ee3a9b4cccd049
abai <mail.contraband@gmail.com>
2020-08-06 11:34:51 -0700

    Fix tips

--- client/src/components/PageTips.vue
+++ client/src/components/PageTips.vue
@@ -6,11 +6,11 @@
 
 <script>
 const tips = [
-  '<b>Note:</b> Snip is protected by <a href="https://www.google.com/recaptcha/about">reCAPTCHA</a>.',
-  '<b>Note:</b> Snip is powered by <a href="https://vuejs.org">Vue</a> & <a href="https://netlify.com">Netlify</a>!',
-  '<b>Note:</b> Snip is created by <a href="https://aiden.codes">Aiden Bai</a>.',
-  '<b>Note:</b> Snip\'s character cap is 50k characters.',
-  '<b>Note:</b> Snip is <a href="https://github.com/aidenybai/snip">open source on Github!</a>'
+  '<b>Tip:</b> Snip is protected by <a target="_blank" href="https://www.google.com/recaptcha/about">reCAPTCHA</a>.',
+  '<b>Tip:</b> Snip is powered by <a target="_blank" href="https://vuejs.org">Vue</a> & <a target="_blank" href="https://netlify.com">Netlify</a>!',
+  '<b>Tip:</b> Snip is created by <a target="_blank" href="https://aiden.codes">Aiden Bai</a>.',
+  '<b>Tip:</b> Snip\'s character cap is 50k characters.',
+  '<b>Tip:</b> Snip is <a target="_blank" href="https://github.com/aidenybai/snip">open source on Github!</a>'
 ]
 
 export default {
