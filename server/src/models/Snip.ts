5cca548ee0bd905904ecf784137f9ad406d3ab9a
abai <mail.contraband@gmail.com>
2020-08-08 21:24:27 -0700

    Reinstate TTL model

--- server/src/models/Snip.ts
+++ server/src/models/Snip.ts
@@ -4,6 +4,10 @@ const Snip = new mongoose.Schema(
   {
     id: { type: String, required: true },
     url: { type: String, required: true },
+    expireAt: {
+      type: Date,
+      default: new Date(Date.now() + 604800000).toISOString(),
+    },
   },
   { timestamps: true }
 );
