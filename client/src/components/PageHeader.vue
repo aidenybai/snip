5076e257754640bb8178dec90ff79462ed33b303
abai <mail.contraband@gmail.com>
2020-08-15 00:19:16 -0700

    Fix icon refs

--- client/src/components/PageHeader.vue
+++ client/src/components/PageHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h3 class="card-title"><font-awesome-icon :icon="['fas', 'hand-scissors']" /> {{ title }}</h3>
+    <h3 class="card-title">
+      <font-awesome-icon :icon="['fas', 'hand-scissors']" /> {{ title }}
+    </h3>
     <h6 class="card-subtitle mb-3 text-muted">{{ subtitle }}</h6>
   </div>
 </template>
