64c398d4c50a4c067052db0cac827e0c8c89a658
abai <mail.contraband@gmail.com>
2020-08-20 23:48:25 -0700

    (assets): change static folder to img

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,9 +4,9 @@ module.exports = {
     themeColor: '#8672E8',
     msTileColor: '#8672E8',
     iconPaths: {
-      favicon32: 'static/icons/favicon-32x32.png',
-      favicon16: 'static/icons/favicon-16x16.png',
-      appleTouchIcon: 'static/icons/apple-touch-icon.png',
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
     },
     manifestOptions: {
       // eslint-disable-next-line @typescript-eslint/camelcase
@@ -14,13 +14,13 @@ module.exports = {
       display: 'minimal-ui',
       icons: [
         {
-          src: './static/icons/android-chrome-192x192.png',
+          src: './img/icons/android-chrome-192x192.png',
           sizes: '192x192',
           type: 'image/png',
           purpose: 'any maskable',
         },
         {
-          src: './static/icons/android-chrome-512x512.png',
+          src: './img/icons/android-chrome-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable',
